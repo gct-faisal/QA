bb7e66f513bbceb957cd7afebc06c048f31016fd
Faisal Humayun <fhumayun@groupcaretech.com>
2015-04-27 14:21:45 -0400

    Basic BDD test

---
+++ src/test/java/RunCukesTest.java
@@ -0,0 +1,16 @@
+/**
+ * Created by fhumayun on 4/27/15.
+ */
+
+/**
+ * Created by fhumayun on 4/13/15.
+ */
+
+import cucumber.api.junit.*;
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+
+@RunWith(Cucumber.class)
+
+public class RunCukesTest {
+}
---
+++ src/test/java/StepDefs.java
@@ -0,0 +1,24 @@
+/**
+ * Created by fhumayun on 4/27/15.
+ */
+import cucumber.annotation.en.*;
+
+public class StepDefs {
+    @Given("^I have \\$(\\d+) in my account$")
+    public void i_have_$_in_my_account(int arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^I wave my magic ring at the machine$")
+    public void i_wave_my_magic_ring_at_the_machine() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^I should get a soda$")
+    public void i_should_get_a_soda() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
